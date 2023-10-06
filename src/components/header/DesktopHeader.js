16a776bec3877dd2a329a74f61f5cdd7b6531dc3
clarefausty <clarefausty@gmail.com>
2023-10-06 03:13:30 +0100

    wip

---
+++ src/components/header/DesktopHeader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import HeaderStyle from "./Header.module.css";
+
+const DesktopHeader = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default DesktopHeader
---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import DesktopHeader from './DesktopHeader'
+
+
+const Header = () => {
+  return (
+    <div>
+        <DesktopHeader/>
+    </div>
+  )
+}
+
+export default Header
