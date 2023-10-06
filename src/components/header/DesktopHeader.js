80c7417e444eb891065ffcd7abb9c016b0c74c0b
clarefausty <clarefausty@gmail.com>
2023-10-06 04:02:46 +0100

    wip

--- src/components/header/DesktopHeader.js
+++ src/components/header/DesktopHeader.js
@@ -1,20 +1,21 @@
 import React from 'react'
-// import HeaderStyle from "./Header.module.css";
+import HeaderStyle from "./Header.module.css";
 
 const DesktopHeader = () => {
+    const headContainer = HeaderStyle.headContainer
   return (
-    <header>
+    <header className={headContainer}>
         <div>
             <h1>Herman Taylor</h1>
             <h2>Author and Public Speaker</h2>
             <button>Book now</button>
             <div>
-               <img src='../assets/facebook-icon.png' alt='Facebook Icon'/>
-               <img src='../assets/instagram-icon.png' alt='Instagram Icon'/>
+               <img src='assets/facebook-icon.png' alt='Facebook Icon'/>
+               <img src='/assets/instagram-icon.png' alt='Instagram Icon'/>
             </div>
-            <div>
-                <img src='../assets/herman-header-image.jpg' alt='Herman Taylor'/>
-            </div>
+        </div>
+        <div>
+            <img src='/assets/herman-header-image.jpg' alt='Herman Taylor'/>
         </div>
     </header>
   )
