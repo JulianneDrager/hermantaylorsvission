392c10de2e106622e0839e98a0b77bd0cb45e8fa
clarefausty <clarefausty@gmail.com>
2023-10-09 09:35:07 +0100

    wip

--- src/components/header/Interview.js
+++ src/components/header/Interview.js
@@ -4,12 +4,17 @@ import StyleInterview from "./Interview.module.css"
 const Interview = () => {
     const interviewSubCon = StyleInterview.interviewSubCon
     const interviewDisplay = StyleInterview.interviewDisplay
+    const interviewCon = StyleInterview.interviewCon
+    const leftCorner = StyleInterview.leftCorner
+    const rightCorner = StyleInterview.rightCorner
     return (
-    <div>
+    <div className={interviewCon}>
+        <img className={leftCorner} src='/assets/left-corner-background.png'alt='left corner background'/>
         <div className={interviewSubCon}>
         <h1><span>Life'S CHAPTER</span><br/>HERMAN'S EXCLUSIVE INTERVIEW</h1>
     <div className={interviewDisplay}></div>
     </div>
+    <img className={rightCorner} src='/assets/right-corner-background.png'alt='left corner background' />
     </div>
   )
 }
