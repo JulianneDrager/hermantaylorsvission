82c47fef30d6fc1f671d0e045e5905b62914fa6b
clarefausty <clarefausty@gmail.com>
2023-10-09 18:46:56 +0100

    wip

--- src/components/header/Interview.js
+++ src/components/header/Interview.js
@@ -8,12 +8,18 @@ const Interview = () => {
     const leftCorner = StyleInterview.leftCorner
     const rightCorner = StyleInterview.rightCorner
     const goldWords = StyleInterview.goldWords
+    const mailCon = StyleInterview.mailCon
     return (
     <div className={interviewCon}>
         <img className={leftCorner} src='/assets/left-corner-background.png'alt='left corner background'/>
         <div className={interviewSubCon}>
         <h1><span className={goldWords}>LIFE'S CHAPTER</span><br/>HERMAN'S EXCLUSIVE INTERVIEW</h1>
     <div className={interviewDisplay}></div>
+    <div className={mailCon}>
+      <img src='/assets/email-icon.png' alt='mail box'/>
+      <h2>CONTACT</h2>
+      <h2>HERMAN@GMAIL.COM</h2>
+    </div>
     </div>
     <img className={rightCorner} src='/assets/right-corner-background.png'alt='left corner background' />
     </div>
