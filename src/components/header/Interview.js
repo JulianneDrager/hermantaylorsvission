22091d64dc1a64f715234aace071c99d589fca80
clarefausty <clarefausty@gmail.com>
2023-10-09 09:09:21 +0100

    wip

--- src/components/header/Interview.js
+++ src/components/header/Interview.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import StyleInterview from "./Interview.module.css"
 
 const Interview = () => {
-  return (
+    const interviewSubCon = StyleInterview.interviewSubCon
+    const interviewDisplay = StyleInterview.interviewDisplay
+    return (
     <div>
-        <div>
+        <div className={interviewSubCon}>
         <h1><span>Life'S CHAPTER</span><br/>HERMAN'S EXCLUSIVE INTERVIEW</h1>
-    <div></div>
+    <div className={interviewDisplay}></div>
     </div>
     </div>
   )
