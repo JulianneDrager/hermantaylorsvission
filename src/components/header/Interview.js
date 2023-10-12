3ba99dcc55a10715037ba1cead675c25e5534bf4
clarefausty <clarefausty@gmail.com>
2023-10-12 14:13:46 +0100

    wip

--- src/components/header/Interview.js
+++ src/components/header/Interview.js
@@ -2,6 +2,7 @@ import React from 'react'
 import StyleInterview from "./Interview.module.css"
 
 const Interview = () => {
+  const currentYear = new Date().getFullYear()
     const interviewSubCon = StyleInterview.interviewSubCon
     const interviewDisplay = StyleInterview.interviewDisplay
     const interviewCon = StyleInterview.interviewCon
@@ -9,6 +10,9 @@ const Interview = () => {
     const rightCorner = StyleInterview.rightCorner
     const goldWords = StyleInterview.goldWords
     const mailCon = StyleInterview.mailCon
+    const mailBox = StyleInterview.mailBox
+    const mailConh2 = StyleInterview.mailConh2
+    const footerDesktop = StyleInterview.footerDesktop
     return (
     <div className={interviewCon}>
         <img className={leftCorner} src='/assets/left-corner-background.png'alt='left corner background'/>
@@ -16,10 +20,11 @@ const Interview = () => {
         <h1><span className={goldWords}>LIFE'S CHAPTER</span><br/>HERMAN'S EXCLUSIVE INTERVIEW</h1>
     <div className={interviewDisplay}></div>
     <div className={mailCon}>
-      <img src='/assets/email-icon.png' alt='mail box'/>
-      <h2>CONTACT</h2>
+      <img className={mailBox} src='/assets/email-icon.png' alt='mail box'/>
+      <h2 className={mailConh2}>CONTACT </h2>
       <h2>HERMAN@GMAIL.COM</h2>
     </div>
+    <footer className={footerDesktop}>COPY RIGHT {currentYear} DESIGNINGGLORY.COM </footer>
     </div>
     <img className={rightCorner} src='/assets/right-corner-background.png'alt='left corner background' />
     </div>
