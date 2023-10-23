5a4818890bf18c03ddcdc71705d6df6f1bb8b342
REBEKAH LEONARD <shelbek@gmail.com>
2023-10-23 15:20:45 -0400

    followed the tutorial and made the changes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,26 @@
 import React from "react";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Image, Button } from "react-bootstrap";
 import HeaderStyle from "./Header.module.css";
 import Article from "./Article";
 import Interview from "./Interview";
+import TitleImg from "../../images/mobile-header-images/mobile-header-title.png"
+import Facebook from "../../images/mobile-header-images/mobile-facebook.png"
+import Instagram from "../../images/mobile-header-images/mobile-instagram.png"
 
 const DesktopHeader = () => {
-  const ColorWhite = { color: "white" };
   const headerImgBack = HeaderStyle.headerImgBack;
   const gradient = HeaderStyle.gradient;
+  const socialBtnSize = HeaderStyle.socialBtnSize;
+  const headerRow = HeaderStyle.headerRow;
+  const socialMediaCol = HeaderStyle.socialMediaCol;
+  const contentH2 = HeaderStyle.contentH2;
+  const headerContent = HeaderStyle.headerContent;
+  const fireBottom = HeaderStyle.fireBottom;
+  const bkcolor = { color: "transparent" };
+  const bkcolor3 = { color: "transparent" };
+  const bkcolor4 = { color: "transparent" };
+  const bkcolor5 = { color: "transparent" };
+
   const headContainer = HeaderStyle.headContainer;
   const headerImageContainer = HeaderStyle.headerImageContainer;
   const headerImage = HeaderStyle.headerImage;
@@ -15,74 +28,62 @@ const DesktopHeader = () => {
   const headSection1 = HeaderStyle.headSection1;
   const headerTitleContainer = HeaderStyle.headerTitleContainer;
   const headerTitle = HeaderStyle.headerTitle;
-  const headerContent = HeaderStyle.headerContent;
+  
   const headerBtn = HeaderStyle.headerBtn;
   const headerBtnContainer = HeaderStyle.headerBtnContainer;
   const headerLinksCon = HeaderStyle.headerLinksCon;
-  const contenth2 = HeaderStyle.contenth2;
+  
   return (
     <header>
       <div className={headContainer}>
         <div className={headSection1}>
           <div className={headerTitleContainer}>
-            <img
-              className={headerTitle}
-              // note on the tutorial I skip to this section, remove the quotes and replace with {}
-              src="/assets/header-title.png"
-              alt="Header Title"
-            />
+            {/* note on the tutorial I skip to this section, remove the quotes and replace with {} */}
+            <Image className={headerTitle} src={TitleImg} alt="Header Title" />
           </div>
           <div className={headerImgBack}>
             <div className={gradient}>
-              <Row>
-                <Col style={ColorWhite}>1</Col>
-                <Col style={ColorWhite}>
-                  2{" "}
+              <Row className={headerRow}>
+                <Col style={bkcolor}>1</Col>
+                <Col className={socialMediaCol}>
                   <div className={headerLinksCon}>
-                    <a href="">
-                      <img src="assets/facebook-icon.png" alt="Facebook Icon" />
+                    <a href="/">
+                      <Image
+                        className={socialBtnSize}
+                        src={Facebook}
+                        alt="Facebook Icon" />
                     </a>
-                    <a href="">
-                      <img
-                        src="/assets/instagram-icon.png"
-                        alt="Instagram Icon"
-                      />
+                    <a href="/">
+                      <Image
+                        className={socialBtnSize}
+                        src={Instagram}
+                        alt="Instagram Icon" />
                     </a>
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col style={bkcolor3}>3</Col>
+                <Col style={bkcolor4}>
+                  4
+                  <div className={headerContent}>
+                    <h2 className={contentH2}> 
+                    AUTHOR & <br /> PUBLIC SPEAKER
+                    </h2>
+                  </div>
+                </Col>
+                <Col style={bkcolor5}>
+                  5
+                  <div className={headerBtnContainer}>
+                    <Button className={headerBtn}>PURCHASE NOW</Button>
                   </div>
                 </Col>
               </Row>
             </div>
           </div>
-          {/* <div className={headerContent}>
-            <h2 className={contenth2}>
-              AUTHOR & <br /> PUBLIC SPEAKER
-            </h2>
-          <div className={headerBtnContainer}>
-            <button className={headerBtn}>BOOK NOW</button>
-          </div>
-          <div className={headerLinksCon}>
-            <a href="">
-              <img src="assets/facebook-icon.png" alt="Facebook Icon" />
-            </a>
-            <a href="">
-              <img src="/assets/instagram-icon.png" alt="Instagram Icon" />
-            </a>
-          </div>
-        </div> */}
         </div>
-        {/* <div className={headerImageContainer}>
-        <img
-          src="/assets/herman-header-image.jpg"
-          alt="Herman Taylor"
-          className={headerImage}
-        />
-        <img
-          src="/assets/header-bottom-corner.png"
-          alt="Herman Taylor"
-          className={bottmBackground}
-        />
-      </div> */}
       </div>
+      <div className={fireBottom}></div>
       {/* <Article />
       <Interview /> */}
     </header>
