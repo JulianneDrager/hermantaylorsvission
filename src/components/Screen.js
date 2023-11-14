ab1e0d5f4662dbdc0304d130be7d381a94385625
clarefausty <clarefausty@gmail.com>
2023-11-14 12:40:27 +0100

    nav section

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -3,16 +3,24 @@ import Header from "./header/Header"
 import Contact from './contact/Contact'
 import InterviewMobile from './interview/InterviewMobile'
 import ArticleMobile from './header/ArticleMobile'
+import { BrowserRouter,  Routes, Route } from 'react-router-dom'
 
 
 const Screen = () => {
   return (
-    <div>
-        <Header/>
-        <InterviewMobile/>
-        <ArticleMobile/>
-        <Contact/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Header/>} />
+      <Route path="/interview" element={<InterviewMobile/>} />
+      <Route path="/article" element={<ArticleMobile/>} />
+      <Route path="/contact" element={<Contact/>} />
+    </Routes>
+    </BrowserRouter>
+        // {/* <Header/>
+        // <InterviewMobile/>
+        // <ArticleMobile/>
+        // <Contact/> */}
+    
   )
 }
 
--- src/components/header/DesktopHeader.js
+++ src/components/header/DesktopHeader.js
@@ -2,6 +2,7 @@ import React from "react";
 import DesktopHeaderStyle from "./desktopHeader.module.css";
 // import Article from "./Article";
 // import Interview from "./Interview";
+import { Link } from "react-router-dom";
 
 const DesktopHeader = () => {
   const headContainer = DesktopHeaderStyle.headContainer;
@@ -17,15 +18,16 @@ const DesktopHeader = () => {
   const headerLinksCon = DesktopHeaderStyle.headerLinksCon;
   const contenth2 = DesktopHeaderStyle.contenth2;
   const navDesktop = DesktopHeaderStyle.navDesktop
+  const linkDesktop = DesktopHeaderStyle.linkDesktop
   const navListDesktop = DesktopHeaderStyle.navListDesktop
   return (
     <header>
       <nav className={navDesktop}>
               <ul className = {navListDesktop}>
-                <li>Home</li>
-                <li>Interview</li>
-                <li>Article</li>
-                <li>Contact</li>
+                <li><Link className={linkDesktop} to="/">Home</Link></li>
+                <li><Link className={linkDesktop} to="/interview">Interview</Link></li>
+                <li><Link className={linkDesktop} to="/article">Article</Link></li>
+                <li><Link className={linkDesktop} to="/contact">Contact</Link></li>
               </ul>
         </nav>
       <div className={headContainer}>
