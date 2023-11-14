e4004094e8e98f9e96abef1567229750be80b7bd
clarefausty <clarefausty@gmail.com>
2023-11-14 16:47:23 +0100

    nav section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@
 
 import React from 'react'
 import Screen from "./components/Screen"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/pages/Layout';
+import Header from './components/header/Header';
+import InterviewMobile from './components/interview/InterviewMobile';
+import ArticleMobile from './components/header/ArticleMobile';
+import Contact from './components/contact/Contact';
 
 const App = () => {
   return (
-    <div>
-      <Screen/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Screen />} />
+          <Route path="home" element={<Header />} />
+          <Route path="interview" element={<InterviewMobile />} />
+          <Route path="article" element={<ArticleMobile />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,30 +1,21 @@
-import React from 'react'
-import Header from "./header/Header"
-import Contact from './contact/Contact'
-import InterviewMobile from './interview/InterviewMobile'
-import ArticleMobile from './header/ArticleMobile'
-import { BrowserRouter,  Routes, Route } from 'react-router-dom'
-import Layout from './pages/Layout'
-
+// Screen.jsx
+import React from 'react';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+import Header from './header/Header';
+import Contact from './contact/Contact';
+import InterviewMobile from './interview/InterviewMobile';
+import ArticleMobile from './header/ArticleMobile';
 
 const Screen = () => {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route element={<Layout/>}>
-      <Route path="/" element={<Header/>} />
-      <Route path="/interview" element={<InterviewMobile/>} />
-      <Route path="/article" element={<ArticleMobile/>} />
-      <Route path="/contact" element={<Contact/>} />
-      </Route>
-    </Routes>
-    </BrowserRouter>
-        // {/* <Header/>
-        // <InterviewMobile/>
-        // <ArticleMobile/>
-        // <Contact/> */}
-    
-  )
+    <div>
+      <Header/>
+      <InterviewMobile/>
+      <ArticleMobile/>
+      <Contact/>
+    </div>
+  );
 }
 
-export default Screen
+export default Screen;
